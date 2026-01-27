5d1100cdd0c0c79313cb7c5a17977fe4a430ca9c
Adam Patterson <hello@adampatterson.ca>
2026-01-27 09:26:58 -0700

    .

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,7 @@ export default defineConfig({
             input: {
                 main: resolve(__dirname, 'index.html'),
                 cover: resolve(__dirname, 'cover/index.html'),
+                // "cover-wip": resolve(__dirname, 'wip/cover.html'),
                 wip: resolve(__dirname, 'wip/index.html'),
             },
         },
