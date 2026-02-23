7c42e0b946b59f7876e47dffb36d7bea30512802
Adam Patterson <hello@adampatterson.ca>
2026-02-23 09:22:34 -0700

    .

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import devtoolsJson from 'vite-plugin-devtools-json'
 import { resolve } from 'node:path'
